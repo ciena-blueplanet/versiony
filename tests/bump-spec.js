8529e3da837886378d785787e740a3912a2d134e
Adam Meadows <adam.meadows@gmail.com>
2017-06-09 06:05:42 -0600

    Fix tests to clean up after themselves

--- tests/bump-spec.js
+++ tests/bump-spec.js
@@ -5,8 +5,6 @@
 
 const utils = require('./utils')
 
-describe('1.2.3', function () {
-  utils.testBump('_package.json', 'patch', '1.2.4')
-  utils.testBump('_package.json', 'minor', '1.3.0')
-  utils.testBump('_package.json', 'major', '2.0.0')
-})
+utils.testBump('1.2.3', 'patch', '1.2.4')
+utils.testBump('1.2.3', 'minor', '1.3.0')
+utils.testBump('1.2.3', 'major', '2.0.0')
--- tests/utils.js
+++ tests/utils.js
@@ -16,12 +16,12 @@ const versiony = require('../index')
  * @returns {String} the full path to the fixture
  */
 function getFullPath (filename) {
-  return path.join(process.cwd(), 'tests', 'fixtures', filename)
+  return path.join(process.cwd(), filename)
 }
 
 /**
  * Create the command we'll pass to exec() to read the current version from a particular config file
- * @param {String} filename - the file to get the version from
+ * @param {String} filename - the filename to get the version from
  * @returns {String} the command to pass to `exec()`
  */
 function createGetVersionCmd (filename) {
@@ -33,47 +33,65 @@ module.exports = {
 
   /**
    * Helper for performing repetative tasks in setting up tests
-   * Since versiony doesn't let you tell it to be quiet (yet), we need to stub out
-   * console.log() but only while _maybeBumpVersion runs, or we won't get mocha output
-   * while running our tests
    *
-   * @param {String} filename - the name of the file to bump
+   * @param {String} version - the starting version
    * @param {String} scope - the scope to bump ('patch', 'minor', or 'major')
    * @param {String} expectedVersion - the expected version after the bump
    */
-  testBump (filename, scope, expectedVersion) {
-    describe(`after a ${scope} bump`, function () {
-      let newVersion
+  testBump (version, scope, expectedVersion) {
+    describe(`when starting from ${version}`, function () {
+      let getVersionCmd, fullPath
       beforeEach(function () {
-        const fullPath = getFullPath(filename)
-        const v = versiony.from(fullPath).indent(' '.repeat(2))
-        switch (scope) {
-          case 'patch':
-            v.patch()
-            break
+        fullPath = getFullPath('_package.json')
+        getVersionCmd = createGetVersionCmd('_package.json')
+        return exec(`echo '{"version": "${version}"}' > ${fullPath}`)
+      })
 
-          case 'minor':
-            v.minor().patch(0)
-            break
+      afterEach(function () {
+        return exec(`rm -rf ${fullPath}`)
+      })
 
-          case 'major':
-            v.newMajor()
-            break
-        }
-
-        v.to(fullPath).end()
-
-        const getVersionCmd = createGetVersionCmd(filename)
-        return exec(`${getVersionCmd}`)
+      it(`should start at ${version}`, function (done) {
+        // This is to make sure the .version() command is working, and to ensure a good state for the
+        // other bump tests (@job13er 2017-06-09)
+        exec(getVersionCmd)
           .then((stdout) => {
-            newVersion = stdout.replace('\n', '')
+            const startingVersion = stdout.replace('\n', '')
+            expect(startingVersion).to.equal(version)
+            done()
           })
       })
 
-      it(`should update the version to ${expectedVersion}`, function () {
-        expect(newVersion).to.be.equal(expectedVersion)
+      describe(`after a ${scope} bump`, function () {
+        let newVersion
+        beforeEach(function () {
+          const v = versiony.from('_package.json').indent(' '.repeat(2))
+          switch (scope) {
+            case 'patch':
+              v.patch()
+              break
+
+            case 'minor':
+              v.minor().patch(0)
+              break
+
+            case 'major':
+              v.newMajor()
+              break
+          }
+
+          v.to('_package.json').end()
+
+          return exec(getVersionCmd)
+            .then((stdout) => {
+              newVersion = stdout.replace('\n', '')
+            })
+        })
+
+        it(`should update the version to ${expectedVersion}`, function () {
+          expect(newVersion).to.be.equal(expectedVersion)
+        })
       })
     })
   }
-
 }
